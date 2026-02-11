1484f67c0d45a8f2fce67f4ec6019d0984aad6af
Marg Zhao <982625148zzj@gmail.com>
2026-02-11 13:51:17 +0100

    added mx shell wrapper and initiate bender config

--- hw/chisel/src/main/scala/snax/streamer/StreamParamGen.scala
+++ hw/chisel/src/main/scala/snax/streamer/StreamParamGen.scala
@@ -2,12 +2,13 @@
 // Solderpad Hardware License, Version 0.51, see LICENSE for details.
 // SPDX-License-Identifier: SHL-0.51
 
-package snax.streamer
 
+package snax.streamer
+ 
 import snax.readerWriter._
 import snax.reqRspManager._
 import snax.utils._
-
+ 
 import chisel3._
 import chisel3.util._
 
@@ -18,72 +19,56 @@ object StreamerParametersGen {
 // constrain: all the reader and writer needs to have same config of crossClockDomain
   def hasCrossClockDomain = false
 
-  def readerParams =
-    Seq(
-      new ReaderWriterParam(
-        spatialBounds       = List(
-          16
-        ),
-        temporalDimension   = 6,
-        tcdmDataWidth       = 64,
-        tcdmSize            = 256,
-        tcdmLogicWordSize   = Seq(256),
-        numChannel          = 16,
-        addressBufferDepth  = 2,
-        dataBufferDepth     = 2,
-        configurableChannel = true,
-        crossClockDomain    = hasCrossClockDomain
+  def readerParams = Seq(
+    new ReaderWriterParam(
+      spatialBounds = List(
+        4
       ),
-      new ReaderWriterParam(
-        spatialBounds       = List(
-          128
-        ),
-        temporalDimension   = 3,
-        tcdmDataWidth       = 64,
-        tcdmSize            = 256,
-        tcdmLogicWordSize   = Seq(256),
-        numChannel          = 128,
-        addressBufferDepth  = 2,
-        dataBufferDepth     = 2,
-        configurableChannel = true,
-        crossClockDomain    = hasCrossClockDomain
+      temporalDimension = 1,
+      tcdmDataWidth = 64,
+      tcdmSize = 128,
+      tcdmLogicWordSize = Seq(256),
+      numChannel = 4,
+      addressBufferDepth = 8,
+      dataBufferDepth = 8,
+      configurableChannel = false,
+      crossClockDomain = hasCrossClockDomain
+   ), 
+    new ReaderWriterParam(
+      spatialBounds = List(
+        4
       ),
-      new ReaderWriterParam(
-        spatialBounds       = List(
-          64
-        ),
-        temporalDimension   = 4,
-        tcdmDataWidth       = 64,
-        tcdmSize            = 256,
-        tcdmLogicWordSize   = Seq(256),
-        numChannel          = 64,
-        addressBufferDepth  = 2,
-        dataBufferDepth     = 2,
-        configurableChannel = true,
-        crossClockDomain    = hasCrossClockDomain
-      )
+      temporalDimension = 1,
+      tcdmDataWidth = 64,
+      tcdmSize = 128,
+      tcdmLogicWordSize = Seq(256),
+      numChannel = 4,
+      addressBufferDepth = 8,
+      dataBufferDepth = 8,
+      configurableChannel = false,
+      crossClockDomain = hasCrossClockDomain
     )
+  )
 
-  def writerParams =
-    Seq(
-      new ReaderWriterParam(
-        spatialBounds       = List(
-          64
-        ),
-        temporalDimension   = 4,
-        tcdmDataWidth       = 64,
-        tcdmSize            = 256,
-        tcdmLogicWordSize   = Seq(256),
-        numChannel          = 64,
-        addressBufferDepth  = 2,
-        dataBufferDepth     = 2,
-        configurableChannel = true,
-        crossClockDomain    = hasCrossClockDomain
-      )
+  def writerParams = Seq(
+    new ReaderWriterParam(
+      spatialBounds = List(
+        4
+      ),
+      temporalDimension = 1,
+      tcdmDataWidth = 64,
+      tcdmSize = 128,
+      tcdmLogicWordSize = Seq(256),
+      numChannel = 4,
+      addressBufferDepth = 8,
+      dataBufferDepth = 8,
+      configurableChannel = false,
+      crossClockDomain = hasCrossClockDomain
     )
+  )
 
   def readerWriterParams = Seq()
 
-  def tagName        = "snax_versacore_"
-  def headerFilepath = "../../target/snitch_cluster/sw/snax/versacore/include"
+  def tagName = "snax_alu_"
+  def headerFilepath = "../../target/snitch_cluster/sw/snax/snax-alu/include"
 }
