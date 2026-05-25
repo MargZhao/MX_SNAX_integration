f1bfe292243ea47018d1fd053c1f4d2d155655d5
Marg Zhao <982625148zzj@gmail.com>
2026-05-25 04:02:00 +0200

    added intro graph draft

--- target/snitch_cluster/sw/apps/snax-mx/data/datagen.py
+++ target/snitch_cluster/sw/apps/snax-mx/data/datagen.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2023 KU Leuven.
+# Copyright 2026 KU Leuven.
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 #
